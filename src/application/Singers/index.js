830cd1d12061695b87099a6b2cc5e79b936f5d69
胡武强 <1358949137@qq.com>
2023-11-06 09:32:25 +0800

    feat: 歌手 List 开发

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,14 +1,27 @@
 import React, { useState, memo } from 'react';
+import { connect } from 'react-redux';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import Horizen from '../../baseUI/horizen-item';
 import Scroll from './../../baseUI/scroll'
-import { categoryTypes, alphaTypes, singerList } from '../../api/mock';
+import { categoryTypes, alphaTypes } from '../../api/mock';
 import { NavContainer, ListContainer, List, ListItem } from './style';
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import {
+  getSingerList,
+  getHotSingerList,
+  changeEnterLoading,
+  changePageCount,
+  refreshMoreSingerList,
+  changePullUpLoading,
+  changePullDownLoading,
+  refreshMoreHotSingerList
+} from './store/actionCreators';
 
-const renderSingerList = () => {
+const renderSingerList = (props) => {
+  const { singerList } = props;
+  const list = singerList ? singerList.toJS(): [];
   return (
     <List>
-      {singerList.map((item, index) => {
+      {list.map((item, index) => {
         return (
           <ListItem key={item.accountId + '' + index}>
             <div className="img_wrapper">
@@ -25,16 +38,20 @@ const renderSingerList = () => {
   )
 }
 
-function Singer() {
+function Singer(props) {
+  const { singerList } = props;
+  const { updateDispatch } = props;
   let [category, setCategory] = useState('');
   let [alpha, setAlpha] = useState('');
 
   let handleUpdateAlpha = (val) => {
     setAlpha(val);
+    updateDispatch(category, val)
   }
 
   let handleUpdateCategory = (val) => {
     setCategory(val);
+    updateDispatch(val, alpha)
   }
   return (
     <NavContainer>
@@ -50,11 +67,50 @@ function Singer() {
         oldVal={alpha} />
       <ListContainer>
         <Scroll>
-          {renderSingerList()}
+          {renderSingerList({singerList})}
         </Scroll>
       </ListContainer>
     </NavContainer>
   )
 }
 
-export default memo(Singer);
+const mapStateToProps = (state) => ({
+  singerList: state.getIn(['singers', 'singerList']),
+  enterLoading: state.getIn(['singers', 'enterLoading']),
+  pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+  pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+  pageCount: state.getIn(['singers', 'pageCount'])
+});
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getHotSingerDispatch() {
+      dispatch(getHotSingerList());
+    },
+    updateDispatch(category, alpha) {
+      dispatch(changePageCount(0));//由于改变了分类，所以pageCount清零
+      dispatch(changeEnterLoading(true));//loading，现在实现控制逻辑，效果实现放到下一节，后面的loading同理
+      dispatch(getSingerList(category, alpha));
+    },
+    // 滑到最底部刷新部分的处理
+    pullUpRefreshDispatch(category, alpha, hot, count) {
+      dispatch(changePullUpLoading(true));
+      dispatch(changePageCount(count + 1));
+      if (hot) {
+        dispatch(refreshMoreHotSingerList());
+      } else {
+        dispatch(refreshMoreSingerList(category, alpha));
+      }
+    },
+    //顶部下拉刷新
+    pullDownRefreshDispatch(category, alpha) {
+      dispatch(changePullDownLoading(true));
+      dispatch(changePageCount(0));//属于重新获取数据
+      if (category === '' && alpha === '') {
+        dispatch(getHotSingerList());
+      } else {
+        dispatch(getSingerList(category, alpha));
+      }
+    }
+  }
+};
+export default connect(mapStateToProps, mapDispatchToProps)(memo(Singer));
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux-immutable';
 import { recommendReducer } from '../application/Recommend/store';
+import { singersReducer } from '../application/Singers/store';
+
 
 export default combineReducers({
   recommend: recommendReducer,
+  singers: singersReducer
 });
